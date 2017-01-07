12e79be0882e5cd85b9acfd45f7f93f0e13a2341
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-01-08 00:34:21 +0200

    changed formatting

--- example.c
+++ example.c
@@ -5,33 +5,31 @@
 static struct tray tray;
 
 static void hello_cb(struct tray_menu *item) {
-	printf("hello cb\n");
-	if (strcmp(tray.icon, "indicator-messages") == 0) {
-		tray.icon = "indicator-messages-new";
-	} else {
-		tray.icon = "indicator-messages";
-	}
-	tray_update(&tray);
+  printf("hello cb\n");
+  if (strcmp(tray.icon, "indicator-messages") == 0) {
+    tray.icon = "indicator-messages-new";
+  } else {
+    tray.icon = "indicator-messages";
+  }
+  tray_update(&tray);
 }
 
 static void quit_cb(struct tray_menu *item) {
-	printf("quit cb\n");
-	tray_exit();
+  printf("quit cb\n");
+  tray_exit();
 }
 
 static struct tray tray = {
-	.icon = "indicator-messages-new",
-	.menu = (struct tray_menu[]){
-		{NULL, "Hello", 0, hello_cb, NULL},
-		{NULL, "Quit", 0, quit_cb, NULL},
-		{NULL, NULL, 0, NULL, NULL}
-	},
+    .icon = "indicator-messages-new",
+    .menu = (struct tray_menu[]){{NULL, "Hello", 0, hello_cb, NULL},
+                                 {NULL, "Quit", 0, quit_cb, NULL},
+                                 {NULL, NULL, 0, NULL, NULL}},
 };
 
 int main(int argc, char *argv[]) {
-	tray_init(&tray);
-	while (tray_loop(1) == 0) {
-		printf("iteration\n");
-	}
-	return 0;
+  tray_init(&tray);
+  while (tray_loop(1) == 0) {
+    printf("iteration\n");
+  }
+  return 0;
 }
--- tray.h
+++ tray.h
@@ -4,18 +4,18 @@
 struct tray_menu;
 
 struct tray {
-	char *icon;
-	char *tooltip;
-	struct tray_menu *menu;
+  char *icon;
+  char *tooltip;
+  struct tray_menu *menu;
 };
 
 struct tray_menu {
-	char *icon;
-	char *text; /* label */
-	int flags;
+  char *icon;
+  char *text; /* label */
+  int flags;
 
-	void (*cb)(struct tray_menu *);
-	void *context;
+  void (*cb)(struct tray_menu *);
+  void *context;
 };
 
 static void tray_update(struct tray *tray);
@@ -31,45 +31,43 @@ static AppIndicator *indicator = NULL;
 static int loop_result = 0;
 
 static void _tray_menu_cb(GtkMenuItem *item, gpointer data) {
-	struct tray_menu *m = (struct tray_menu *) data;
-	m->cb(m);
+  struct tray_menu *m = (struct tray_menu *)data;
+  m->cb(m);
 }
 
 static int tray_init(struct tray *tray) {
-	if (gtk_init_check(0, NULL) == FALSE) {
-		return -1;
-	}
-	indicator = app_indicator_new(TRAY_APPINDICATOR_ID, tray->icon,
-			APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
-	app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
-	tray_update(tray);
-	return 0;
+  if (gtk_init_check(0, NULL) == FALSE) {
+    return -1;
+  }
+  indicator = app_indicator_new(TRAY_APPINDICATOR_ID, tray->icon,
+                                APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
+  app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
+  tray_update(tray);
+  return 0;
 }
 
 static int tray_loop(int blocking) {
-	gtk_main_iteration_do(blocking);
-	return loop_result;
+  gtk_main_iteration_do(blocking);
+  return loop_result;
 }
 
 static void tray_update(struct tray *tray) {
   struct tray_menu *m;
 
-	app_indicator_set_icon(indicator, tray->icon);
+  app_indicator_set_icon(indicator, tray->icon);
   GtkMenuShell *gtk_menu = (GtkMenuShell *)gtk_menu_new();
   for (struct tray_menu *m = tray->menu; m != NULL && m->text != NULL; m++) {
     GtkWidget *item = gtk_menu_item_new_with_label(m->text);
     gtk_widget_show(item);
     gtk_menu_shell_append(GTK_MENU_SHELL(gtk_menu), item);
-		if (m->cb != NULL) {
-			g_signal_connect(item, "activate", G_CALLBACK(_tray_menu_cb), m);
-		}
+    if (m->cb != NULL) {
+      g_signal_connect(item, "activate", G_CALLBACK(_tray_menu_cb), m);
+    }
   }
   app_indicator_set_menu(indicator, GTK_MENU(gtk_menu));
 }
 
-static void tray_exit() {
-	loop_result = -1;
-}
+static void tray_exit() { loop_result = -1; }
 
 #elif defined(TRAY_APPKIT)
 
@@ -80,13 +78,13 @@ static NSStatusBar *statusBar;
 static id statusItem;
 static id statusBarButton;
 
-@interface Tray : NSObject<NSApplicationDelegate>
-- (void) menuCallback: (id) sender;
+@interface Tray : NSObject <NSApplicationDelegate>
+- (void)menuCallback:(id)sender;
 @end
 @implementation Tray
-- (void) menuCallback: (id) sender {
-  struct tray_menu *m = (struct tray_menu *)
-   [[sender representedObject] pointerValue];
+- (void)menuCallback:(id)sender {
+  struct tray_menu *m =
+      (struct tray_menu *)[[sender representedObject] pointerValue];
   m->cb(m);
 }
 @end
@@ -96,7 +94,7 @@ static int tray_init(struct tray *tray) {
   [NSApplication sharedApplication];
 
   Tray *trayDelegate = [Tray new];
-  [NSApp setDelegate: trayDelegate];
+  [NSApp setDelegate:trayDelegate];
 
   statusBar = [NSStatusBar systemStatusBar];
   statusItem = [statusBar statusItemWithLength:NSVariableStatusItemLength];
@@ -113,9 +111,9 @@ static int tray_loop(int blocking) {
   NSEvent *event;
   NSDate *until = (blocking ? [NSDate distantFuture] : [NSDate distantPast]);
   event = [NSApp nextEventMatchingMask:NSAnyEventMask
-    untilDate:until
-    inMode:NSDefaultRunLoopMode
-    dequeue:YES];
+                             untilDate:until
+                                inMode:NSDefaultRunLoopMode
+                               dequeue:YES];
   if (event) {
     [NSApp sendEvent:event];
   }
@@ -131,12 +129,11 @@ static void tray_update(struct tray *tray) {
   for (struct tray_menu *m = tray->menu; m != NULL && m->text != NULL; m++) {
     NSMenuItem *menuItem = [NSMenuItem alloc];
     [menuItem autorelease];
-    [menuItem
-      initWithTitle: [NSString stringWithUTF8String: m->text]
-      action:@selector(menuCallback:)
-      keyEquivalent:@""];
+    [menuItem initWithTitle:[NSString stringWithUTF8String:m->text]
+                     action:@selector(menuCallback:)
+              keyEquivalent:@""];
     [menuItem setEnabled:YES];
-    [menuItem setRepresentedObject: [NSValue valueWithPointer:m]];
+    [menuItem setRepresentedObject:[NSValue valueWithPointer:m]];
 
     [menu addItem:menuItem];
 
@@ -146,9 +143,7 @@ static void tray_update(struct tray *tray) {
   [statusItem setMenu:menu];
 }
 
-static void tray_exit() {
-  [NSApp terminate:NSApp];
-}
+static void tray_exit() { [NSApp terminate:NSApp]; }
 
 #elif defined(TRAY_WINAPI)
 #else
